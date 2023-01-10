e240593d002d53d41d0a2bfc4508714640cb1e25
achille56 <leclercqjm59@gmail.com>
2023-01-10 15:07:46 +0100

    exo 136

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
 
 // Premiere ligne
-echo 10 5;
+$n1 = 10;
+$n2 = 5;
+echo $n1 * $n2 . "<br>";
 
 //Deuxieme ligne
-echo 10 2;
+$n3 = 10;
+$n4 = 2;
+echo $n3 * $n4 . "<br>";
 
 //Troisieme ligne
-if($a $b) {
-    echo "<br><br>a est identique à b";
+$a = 2;
+$b = 2;
+
+if($a == $b) {
+    echo "<br><br>a est identique à b" . "<br>";
 }
 
 //Quatrieme ligne
-if($a $b) {
+if($a != $b) {
     echo"<br><br>a n'est pas identique à b";
 }
 
@@ -20,7 +27,10 @@ if($a $b) {
 //Cinquieme ligne
 $arr1 = ["machin", "truc"];
 $arr2 = ["truc", "machin"];
-if($arr1 $arr2) {
+if($arr1 == $arr2) {
     echo "<br><br>les tableaux ont le même contenu";
+}
+else {
+    echo "les tableaux ont pas le meme contenu";
 }
 
